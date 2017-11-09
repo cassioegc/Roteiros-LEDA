ea1aa39045c17efa60d3cd03e1e651ab3cac602e
cassioegc <cassio.cordeiro@ccc.ufcg.edu.br>
2017-11-08 22:23:34 -0300

    Roteiro Leda

--- R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
+++ R04-01-Rot-Sorting-Gnome-e-Comb-environment/src/main/java/sorting/variationsOfBubblesort/GnomeSort.java
@@ -10,13 +10,10 @@ public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	public void sort(T[] array, int leftIndex, int rightIndex) {
 		if (array != null && leftIndex >= 0 && rightIndex < array.length && rightIndex >= leftIndex
 				&& array.length != 0) {
-			int pivot = leftIndex + 1;
+			int pivot = leftIndex;
 			
-			while (pivot < rightIndex - 1) {
+			while (pivot < rightIndex) {
 				
-				if (pivot == leftIndex) {
-					pivot++;
-				}
 				if (array[pivot+1].compareTo(array[pivot]) >= 0) {
 					pivot++;
 				}
@@ -24,7 +21,16 @@ public class GnomeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 					Util.swap(array, pivot, pivot+1);
 					pivot--;
 				}
+				if (pivot == leftIndex-1) {
+					pivot++;
+				}
 			}
 		}
+	}
+	public static void main(String[] args) {
+		AbstractSorting<Integer> s = new GnomeSort<>();
+		Integer[] a = new Integer[] { 30, 28, 7, 29, 11, 26, 4, 22, 23,
+				1 };
+		s.sort(a,0,9);
 	}
 }
